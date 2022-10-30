fd08575e37a7f277c6cc6f83bc97607a7baf7cf5
Tyler Sadler <tsadler@ualberta.ca>
2022-10-30 15:13:37 -0600

    Added hasCity signature, create hasCityTest.

--- app/src/test/java/com/example/lab8/CustomListTest.java
+++ app/src/test/java/com/example/lab8/CustomListTest.java
@@ -2,6 +2,8 @@ package com.example.lab8;
 
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 
 //import org.junit.Before;
@@ -24,7 +26,18 @@ public class CustomListTest {
     }
 
     /**
-     * Test the {@link CustomList#addCity} function of {@link CustomList}. Check
+     * A simple helper method for tests to create a mock {@link City} for
+     * use in testing.
+     *
+     * @return A {@link City} with Edmonton as city name and Alberta as province
+     * name.
+     */
+    public City MockCity() {
+        return new City("Edmonton", "Alberta");
+    }
+
+    /**
+     * Test the {@link CustomList#addCity} method of {@link CustomList}. Check
      * the size of the list increases, which indicates city added.
      */
     @Test
@@ -33,5 +46,19 @@ public class CustomListTest {
         int numCities = list.getCount();
         list.addCity(new City("Edmonton", "Alberta"));
         assertEquals(list.getCount(), numCities+1);
+    }
+
+    /**
+     * Test the {@link CustomList#hasCity} method of {@link CustomList}. Add
+     * a city, check that it was successfully added with hasCity. First check
+     * list does not contain the city before adding.
+     */
+    @Test
+    public void hasCityTest() {
+        list = MockCustomList();
+        City city = MockCity();
+        assertFalse(list.hasCity(city));
+        list.addCity(city);
+        assertTrue(list.hasCity(city));
     }
 }
