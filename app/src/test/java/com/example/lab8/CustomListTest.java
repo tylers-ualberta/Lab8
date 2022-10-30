3961b8ffd4a57af871907e2a9cde4de3c258f47f
Tyler Sadler <tsadler@ualberta.ca>
2022-10-30 15:03:40 -0600

    Implement addCityTest

--- app/src/test/java/com/example/lab8/CustomListTest.java
+++ app/src/test/java/com/example/lab8/CustomListTest.java
@@ -11,6 +11,27 @@ import org.junit.jupiter.api.Test;
 import java.util.ArrayList;
 
 public class CustomListTest {
+    private CustomList list;
 
+    /**
+     * A simple helper method for tests to create a mock {@link CustomList} for
+     * use in testing.
+     *
+     * @return A {@link CustomList} with an empty array and null context.
+     */
+    public CustomList MockCustomList() {
+        return new CustomList(null, new ArrayList<>());
+    }
 
+    /**
+     * Test the {@link CustomList#addCity} function of {@link CustomList}. Check
+     * the size of the list increases, which indicates city added.
+     */
+    @Test
+    public void addCityTest() {
+        list = MockCustomList();
+        int numCities = list.getCount();
+        list.addCity(new City("Edmonton", "Alberta"));
+        assertEquals(list.getCount(), numCities+1);
+    }
 }
